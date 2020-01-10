250e30e0ec9978a1dc368c6b1d71eee7148fd96f
matthew29tang <matthew29tang@gmail.com>
2020-01-09 16:24:01 -0800

    Add web server for PDF to TeX

---
+++ src/main.py
@@ -0,0 +1,51 @@
+import os
+from app import app
+from flask import Flask, Response, request, redirect, render_template
+import util
+
+DIR = os.path.dirname(os.path.abspath(__file__)) + "\\"
+
+
+@app.route("/", methods=["POST", "GET"])
+def upload():
+    if request.method == "POST":
+        for key, f in request.files.items():
+            if key.startswith("file"):
+                f.save(os.path.join(app.config["UPLOADED_PATH"], f.filename))
+    return render_template("upload.html")
+
+
+@app.route("/complete", methods=["POST", "GET"])
+def complete():
+    if request.referrer is None:
+        return render_template("error.html")
+
+    if len(os.listdir(app.config["UPLOADED_PATH"])) != 1:
+        return redirect("/", code=302)
+
+    filename = os.listdir(app.config["UPLOADED_PATH"])[0]
+    latex = util.convertPdf(app.config["UPLOADED_PATH"] + "\\" + filename)
+    texFile = DIR + "util/" + filename.split("\\")[-1].split(".")[0] + ".tex"
+    a = open(texFile, "r")
+    tex = a.read()
+    a.close()
+    os.remove(texFile)
+    filelist = [
+        f for f in os.listdir(app.config["UPLOADED_PATH"]) if f.endswith(".pdf")
+    ]
+    for f in filelist:
+        os.remove(os.path.join(app.config["UPLOADED_PATH"], f))
+
+    return Response(
+        tex,
+        mimetype="text",
+        headers={
+            "Content-disposition": "attachment; filename="
+            + filename.split("\\")[-1].split(".")[0]
+            + ".tex"
+        },
+    )
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/util/pdfconvert.py
@@ -0,0 +1,73 @@
+import os, cv2
+import numpy as np
+import pdf2image as pdfIm
+from . import detect
+
+DEBUG = False
+SCALE = 2
+DIR = os.path.dirname(os.path.abspath(__file__)) + "\\"
+
+
+def convertPdf(pdf):
+    pages = pdfIm.convert_from_path(pdf)
+    totalPieces = []
+    for i in range(len(pages)):
+        # print(DIR + str(i) + ".jpg")
+        pages[i].save(DIR + str(i) + ".jpg", "JPEG")
+        totalPieces += _findBoxes(str(i))
+    print(totalPieces)
+    latex = [detect.ocrServer(piece) for piece in totalPieces]
+    text = "\n\n".join(latex)
+    text = text.replace("\n", "\n\n")
+    text = text.replace("\\(", "$")
+    text = text.replace("\\)", "$")
+    a = open(DIR + pdf.split("\\")[-1].split(".")[0] + ".tex", "w")
+    a.write(text)
+    a.close()
+    return text
+
+
+def _findBoxes(filename):
+    image = cv2.imread(DIR + filename + ".jpg")
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (7, 7), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    dilate = cv2.dilate(thresh, kernel, iterations=7)
+
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+    pieces = []
+    for i in range(len(cnts) - 1, -1, -1):
+        x, y, w, h = cv2.boundingRect(cnts[i])
+        if w * h > 20000:
+            # cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            part = image[y : y + h, x : x + w]
+            pieceName = (
+                DIR + "pieces/" + filename + "_" + str(len(cnts) - 1 - i) + ".jpg"
+            )
+            cv2.imwrite(pieceName, part)
+            pieces.append(pieceName)
+
+    try:
+        os.remove(DIR + filename + ".jpg")
+    except:
+        pass
+
+    if DEBUG:
+        cv2.imshow("thresh", thresh)
+        cv2.imshow("dilate", dilate)
+        cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+        cv2.imshow("image", image)
+        cv2.resizeWindow(
+            "image", (int(image.shape[1] / SCALE), int(image.shape[0] / SCALE))
+        )
+        cv2.waitKey()
+    return pieces
+
+
+if __name__ == "__main__":
+    convert()
